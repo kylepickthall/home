7456c97036aa01725159f170e084d562e697bda9
kylepickthall <66224516+kylepickthall@users.noreply.github.com>
2024-06-10 14:36:48 -0700

    Update shaders.js

--- plotter/shaders.js
+++ plotter/shaders.js
@@ -1,8 +1,11 @@
 const vertexShaderCode = `
 	attribute float size;
+	attribute float isFlat;
 	varying vec3 vPosition;
+	varying float vIsFlat;
 	void main() {
 		vPosition = position;
+		vIsFlat = isFlat;
 		vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
 		gl_PointSize = size * (300.0 / -mvPosition.z);
 		gl_Position = projectionMatrix * mvPosition;
@@ -16,6 +19,7 @@ const fragmentShaderCode = `
 	uniform bool uToggleSectioning;
 	uniform float uSectioningThreshold;
 	varying vec3 vPosition;
+	varying float vIsFlat;
 	void main() {
 		if (uToggleSectioning) {
 			if (vPosition.z < uSectioningThreshold) {
@@ -28,6 +32,10 @@ const fragmentShaderCode = `
 			float range = uMaxVals.z - uMinVals.z;
 			float colorVal = (vPosition.z - uMinVals.z) / range;
 			color = vec3(1.0 - colorVal, colorVal, colorVal * 0.5);
+		}
+		
+		if (vIsFlat == 1.0) {
+			color = vec3(0.2, 0.4, 1.0);
 		}
 
 		gl_FragColor = vec4(color, 1.0);
